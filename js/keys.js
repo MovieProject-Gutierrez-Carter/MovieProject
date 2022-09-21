47235952cded32d6176506f4ffba5090ae993f3c
Christian Carter <carter.lee2028@gmail.com>
2022-09-21 14:45:41 -0500

    movie exercise progress

---
+++ js/keys.js
@@ -0,0 +1,2 @@
+const MOVIE_API_KEY = 'f077797c';
+const THE_MOVIE_API_KEY = '15d2ea6d0dc1d476efbca3eba2b9bbfb';
--- js/movies.js
+++ js/movies.js
@@ -3,28 +3,53 @@
 //below is the link to the glitch http site
 //https://saber-dear-spoonbill.glitch.me/
 
-fetch('https://saber-dear-spoonbill.glitch.me/books').then((data) =>{
-    // //json format
-    // console.log(data);
-    return data.json()
+fetch('https://uncovered-real-smartphone.glitch.me//movies').then((data) =>{
+// //json format
+// console.log(data);
+return data.json()
 }).then(data => {
-    console.log(data[0].author);
-    let tableData = "";
-    data.map(book => {
-        console.log(book.title.includes("Dune"));
-        // //This can be used when we need to filter through genre
-        // if(book.title.includes("Dune"))
-        // {
-        //     tableData += ` <tr>
-        //                     <td>${book.title}</td>
-        //                     <td>${book.author}</td>
-        //                </tr>`
-        // }
 
-        tableData += ` <tr>
-                            <td>${book.title}</td>
-                            <td>${book.author}</td>
-                       </tr>`
-    });
-    $('tbody').html(tableData);
+    // console.log(data);
+    let tableData = "";
+    data.map(movie => {
+        let img = "";
+
+        //create a fetch call to themoviedb.org api to capture the poster path
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${THE_MOVIE_API_KEY}&query=${movie.title}`).then((data) =>{
+        // //json format
+        // console.log(data);
+        return data.json()
+    }).then(data => {
+
+        //create a variable to edith the img field in our glitch server
+        const editImg = {
+            "img": data.results[0].poster_path
+        };
+
+        //update data
+        fetch(`https://uncovered-real-smartphone.glitch.me/movies/${movie.id}`, {
+        method: 'PATCH',
+            headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editImg),
+    }).then(() => fetch('https://uncovered-real-smartphone.glitch.me/movies').then(resp => resp.json()).then(data => console.log(data)));
+
+}).catch(error => console.error(error));
+
+tableData +=  `<tr>
+    <td class="border border_info">${movie.title}</td>
+    <td class="border border_info">${movie.director}</td>
+    <td class="border border_info">${movie.year}</td>
+    <td class="border border_info">${movie.rating}</td>
+    <td class="border border_info">${movie.genre}</td>
+    <td class="border width-100"><img  id="poster" src="https://image.tmdb.org/t/p/original${movie.img}" alt="movie poster"></td>
+             </tr>`
+console.log(movie.img);
 });
+$('tbody').html(tableData);
+$('#loading').css('visibility', 'hidden')
+}).catch(error => console.error(error));
+
+
+
